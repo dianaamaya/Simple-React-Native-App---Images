30af3c4f3f23abc1a0b5f92616dc0839b4a45298
diana <dianaamayarojas@gmail.com>
2021-01-06 11:53:18 +0100

    init

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  StyleSheet, Text, View, Image, Button, TouchableOpacity, Platform,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
 
-export default function App() {
+const App = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access to the camera is required');
+    }
+    
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+  
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      const remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri)
+      setSelectedImage({ uri: pickerResult.uri, remoteUri: remoteUri })
+    } else {
+      setSelectedImage({ uri: pickerResult.uri })
+    }
+  
+  };
+
+  const openShareDialog = async () => {
+    if (!await Sharing.isAvailableAsync()) {
+      alert(`Image is avaliable at: ${selectedImage.remoteUri}`);
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Pick an Image!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image source={{ uri: selectedImage !== null ? selectedImage.uri : "https://picsum.photos/200/200", }}
+               style={styles.image} />
+      </TouchableOpacity>
+      {selectedImage ?
+      (<Button onPress={openShareDialog} color="deepskyblue" title="Share this image" />)
+      : (<View></View>)}
     </View>
   );
 }
@@ -14,8 +60,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginTop: 20,
+    marginBottom: 20,
+  }
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,31 @@
+# Simple React Native App - Images.
+
+This application allows users to select an image from the phone or computer and share it through social networks. The App was implemented with React Native.
+
+## INSTALATION
+
+Install dependencies, in main folder write: npm install 
+
+## RUN PROJECT
+
+npm start - to run the project
+
+## BUILD
+
+- expo build:android
+- expo build:ios
+- expo build:web
+
+## STRUCTURE
+
+- assets contains all required media
+- App.js contains the application
+
+## DESCRIPTION
+
+**Actions:**
+
+- User should allow the App access to the gallery
+- User can select an Image from the phone/computer gallery
+- User can see the selected image on the screen
+- User can share this image through social networks
